1552d4091128edcedb7b0576d0cf43aca080e13a
Sebastian Dresler <224800@edu.p.lodz.pl>
2021-11-29 15:38:57 +0100

    Working settings

---
+++ Astro/app/src/main/java/com/example/astro/Menu.java
@@ -0,0 +1,43 @@
+package com.example.astro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class Menu extends Fragment implements View.OnClickListener {
+
+    private EditText latitude, longitude, refreshInterval;
+    private TextView settings;
+    public Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        latitude = view.findViewById(R.id.latitude);
+        longitude = view.findViewById(R.id.longitude);
+        refreshInterval = view.findViewById(R.id.refreshInterval);
+        settings = view.findViewById(R.id.actualSettings);
+        button = view.findViewById(R.id.submit);
+        button.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        MainActivity.latitude =latitude.getText().toString();
+        MainActivity.longitude =longitude.getText().toString();
+        MainActivity.refreshInterval = refreshInterval.getText().toString();
+        settings.setText("Aktualne ustawienia:\nSzerokość geograficzna: " +MainActivity.latitude+"\nDługość geograficzna: "+MainActivity.longitude+"\nCzas odświeżania: "+MainActivity.refreshInterval);
+    }
+
+
+
+}
